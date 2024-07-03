dd07228acecc7577942539bf31ec69eae2956e66
admin <mdyusufbinnur@gmail.com>
2024-07-03 10:28:49 +0600

    Search Update

--- components/Category/index.vue
+++ components/Category/index.vue
@@ -6,7 +6,13 @@
           {{ $t('browse_all_categories') }}
         </div>
       </v-col>
-      <v-col :cols="bp.mdAndUp ? 2 : 4" v-for="(cat, i) in categories" :key="i">
+      <v-col v-if="loading" cols="2"  v-for="i in 18" :key="i">
+        <v-skeleton-loader
+          v-bind="attrs"
+          type="card-avatar"
+        ></v-skeleton-loader>
+      </v-col>
+      <v-col v-if="!loading" :cols="bp.mdAndUp ? 2 : 4" v-for="(cat, i) in categories" :key="i">
         <v-card :elevation="0" class="text-center" @click.prevent="$router.push('/products?category='+cat.id)">
           <v-img :src="cat.image" contain :aspect-ratio="bp.mdAndUp ? 6 : 4"/>
           <v-card-text class="pa-1">
@@ -23,62 +29,39 @@
 </template>
 
 <script>
-import education from 'static/public/category/book-open-cover.png'
-import car from 'static/public/category/car-rear.png'
-import pet from 'static/public/category/cat.png'
-import sports from 'static/public/category/dice-d12.png'
-import job from 'static/public/category/employee-man.png'
-import living from 'static/public/category/fireplace.png'
-import property from 'static/public/category/property.png'
-import electronic from 'static/public/category/tv-music.png'
-import mobile from 'static/public/category/mobile-button.png'
-import fashion from 'static/public/category/tshirt.png'
-import women from 'static/public/category/shirt-tank-top.png'
-import business from 'static/public/category/suitcase-alt.png'
-import essential from 'static/public/category/tools.png'
-import agriculture from 'static/public/category/wheat-awn.png'
-import overseas from 'static/public/category/airplan.png'
-import service from 'static/public/category/user-headset.png'
-import {mapGetters} from "vuex";
 
 export default {
   name: "Category",
   data() {
     return {
       categories: [],
-      loading: false
+      loading: false,
+      attrs: {
+        class: 'mb-6',
+        boilerplate: false,
+        elevation: 0,
+      },
     }
   },
-  computed: {
-    ...mapGetters({
-      categoryList: 'category/getCategories'
-    })
-  },
 
-  watch: {
-    categoryList: {
-      handler(nv, ov) {
-        if (this.categoryList && this.categoryList.length) {
-          this.categories = JSON.parse(JSON.stringify(this.categoryList))
-        }
-      },
-      immediate: true,
-      deep: true
-    },
-  },
   methods: {
     init() {
       this.loading = true
-      this.$store.dispatch('category/init')
-        .finally(() => {
-          this.loading = false
+      this.$axios
+        .get('category-list')
+        .then((response) => {
+          this.categories = response.data.data
         })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+        this.loading = false
+      })
     },
   },
   created() {
-    if (!this.categories.length) {
-      this.init()
-    }
+    this.init()
   }
 }
 </script>
